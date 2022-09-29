105b7acd7e0d95d884570f8d304c7aab7e8ecfbd
Raphy Mejia <moyeto3@gmail.com>
2022-09-29 13:06:51 -0700

    product,foodtype  y tipado de los Jsons

---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id:        number;
+  name:      string;
+  price:     string;
+  picture:   null | string;
+  food_type: null | string;
+}
+export interface Footype {
+  id:   number;
+  name: string;
+}
